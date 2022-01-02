e4b7aa48b692a0cddae2c787bed25d7a0f2e3a20
CroogQT <croog@queuetimes.com>
2022-01-01 16:45:22 -0800

    reusing admin page code for https redirect

---
+++ src/certs.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+
+const certs = {
+
+    getHTTPSCredentials(domain){
+
+        const credentialDirectoryPath = `/etc/letsenctypt/live/${domain}`
+
+        const credentials = {
+
+            key: fs.readFileSync(`${credentialDirectoryPath}/privkey.pem`, 'utf-8'),
+            cert: fs.readFileSync(`${credentialDirectoryPath}fullchain.pem`, 'utf-8')
+
+        }
+
+        return credentials
+
+    },
+
+
+    redirectToHTTPS(req, res, next) {
+
+        if (!req.secure) {
+
+            return res.redirect(`https://${req.headers.host}${req.url}`)
+
+        }
+
+        next()
+
+    }
+
+}
+
+export default certs;
