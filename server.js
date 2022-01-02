159364ed75ec3ad4969e4be853e6f7cb4875393e
CroogQT <croog@queuetimes.com>
2022-01-01 16:57:21 -0800

    initial template server

---
+++ server.js
@@ -0,0 +1,27 @@
+import http from 'http';
+import https from 'https';
+import express from 'express';
+import certs from './src/certs.js';
+import envTool from './src/envTool.js';
+import config from './config.js';
+
+const app = express();
+const isEnvProd = envTool.isProd(process)
+
+if (isEnvProd) {
+
+    certs.redirectToHTTPS();
+
+}
+
+
+
+if(isEnvProd){
+
+    const httpsServer = https.createServer(certs.getHTTPSCredentials('karimkhalil.com'), app);
+    httpsServer.listen(config.server.sslPort);
+
+}
+
+const httpServer = http.createServer(app);
+httpServer.listen(config.server.httpPort);
